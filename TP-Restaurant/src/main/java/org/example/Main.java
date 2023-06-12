278b0387c116aaf476632594e11ba41e69e79dbb
Agustín Correa Pasini <agus98-11@hotmail.com>
2023-06-12 13:41:42 -0300

    agregando uso API

--- TP-Restaurant/src/main/java/org/example/Main.java
+++ TP-Restaurant/src/main/java/org/example/Main.java
@@ -1,15 +1,46 @@
 package org.example;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)  {
 
+       try {
+
+           URL url = new URL("https://referential.p.rapidapi.com/v1/city?ip=190.244.103.68");
+
+           HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+           conn.setRequestMethod("GET");
+           conn.connect();
+
+           int responseCode = conn.getResponseCode();
+           if (responseCode != 200){
+                throw new RuntimeException("Ocurrio un error " + responseCode);
+           }else{
+                StringBuilder informacion = new StringBuilder();
+                Scanner sc3 = new Scanner(url.openStream());
+
+                while (sc3.hasNext()){
+                    informacion.append(sc3.nextLine());
+                }
+
+                sc3.close();
+
+               System.out.println(informacion);
+           }
+
+       }catch (Exception eee) {
+           eee.printStackTrace();
+       }
 
        Restaurant res = new Restaurant();
        res.setearMozos();
        res.setearPlatos();
        res.setearMesas();
 
-        Integer aux = null;
+       Integer aux = null;
 
         try {
             aux = res.ingresarYBuscarMesa();
